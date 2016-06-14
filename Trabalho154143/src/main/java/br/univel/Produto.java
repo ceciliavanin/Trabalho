947aabc74626f274052e5333c42dd2cdd195247b
Cecilia Vanin <ceciliaavanin@gmail.com>
2016-06-14 20:29:38 -0300

    Classe produto.

--- Trabalho154143/src/main/java/br/univel/Produto.java
+++ Trabalho154143/src/main/java/br/univel/Produto.java
@@ -1,5 +1,37 @@
 package br.univel;
 
-public class Produto {
+import java.math.BigDecimal;
 
+public class Produto {
+	public Produto(int id, String descricao, BigDecimal valor) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+	
+	
+	int id;
+	String descricao;
+	BigDecimal valor;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
 }
