e2dd130073b30cb2f224ac426b5e0d352c9fa098
Cecilia Vanin <ceciliaavanin@gmail.com>
2016-06-21 18:20:23 -0300

    Teste

--- Trabalho154143/src/main/java/br/univel/RealizaVenda.java
+++ Trabalho154143/src/main/java/br/univel/RealizaVenda.java
@@ -18,21 +18,27 @@ public class RealizaVenda {
 		
 		Produto p1 = new Produto(id,nome, valor);
 		lista.add(p1);
-		
-		
+		v.setValortotal(somavenda(lista));
+		BigDecimal valorRecebido = null;;
+		v.setValortroco(troco(valorRecebido ,v.getValortotal()));
 	}
 			
-	public void somavenda(List<Produto> lista){
-		Venda v = new Venda();
-		
+	public static BigDecimal troco(BigDecimal valorRecebido, BigDecimal valortotal) {
+		BigDecimal valorTroco = null;
+		valorTroco = (valorRecebido.subtract(valortotal));
+		return valorTroco;
+	}
+
+	public BigDecimal somavenda(List<Produto> lista){
+	
 		BigDecimal valor = null;
 		for (Produto l: lista){
 			valor = (l.getValor().add(valor));
 		}
-		v.setValortotal(valor);
-		BigDecimal valorRecebido = null;
-		v.setValortroco(valorRecebido.subtract(v.getValortotal()));
+		return valor;
 				
+	
 	}
+	
 }
 
--- Trabalho154143/src/test/java/RealizaVendaTest.java
+++ Trabalho154143/src/test/java/RealizaVendaTest.java
@@ -1,4 +1,53 @@
-@Test
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import br.univel.RealizaVenda;
+
+
+@RunWith(value = Parameterized.class)
 public class RealizaVendaTest {
+
+	private BigDecimal valorTotal;
+	private BigDecimal valorTroco;
+	private BigDecimal valorRecebido;
+
+	
+	
+
+	public RealizaVendaTest(BigDecimal valorRecebido, BigDecimal valorTotal, BigDecimal valorTroco) {
+		
+		this.valorTotal = valorTotal;
+		this.valorTroco = valorTroco;
+		this.valorRecebido = valorRecebido;
+	}
+
+	
+	@Parameters(name = "Teste {index}: éprasomar({0}-{1})={2}")
+	public static Iterable<Object[]> data1() {
+		return Arrays.asList(new Object[][] { 
+			{ 120, 110, 10 }, 
+			{ 12, 9, 3 }, 
+			{ 20, 10, 10 }, 
+			{ 41, 22, 19 }, 
+			{ 10, 10, 0 }			
+		});
+	}
+
+	@Test
+	public void test_sub() {	
+		assertEquals(valorTroco, RealizaVenda.troco(valorRecebido, valorTotal));
+	}
+	@Test
+	public void testaTroco(){
+		assertEquals(valorTroco, RealizaVenda.troco(valorRecebido, valorTotal));
+	}
+	
 
 }
