3e794b241fa7f1112c052113e5d250a8e62eb625
Vanessa Panosso <panossovanessa@gmail.com>
2016-06-26 23:10:17 -0300

    Conexao Banco

--- Trabalho154143/src/main/java/br/univel/banco/ConexaoBanco.java
+++ Trabalho154143/src/main/java/br/univel/banco/ConexaoBanco.java
@@ -2,22 +2,25 @@ package br.univel.banco;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 public class ConexaoBanco {
 
 		public static Properties getProp() throws IOException {
 			Properties props = new Properties();
 			FileInputStream file = new FileInputStream(
-					"./properties/dados.properties");
+					"properties/banco.properties");
 			props.load(file);
 			return props;
 
 		}
 
-		public static void  main(String args[]) throws IOException {
+		public Connection abrirConexao() throws IOException {
 			String login; 
 			String url; 
-			String password; //Variável que guardará o password do usúario.
+			String password; 
 			System.out.println("************Teste de leitura do arquivo de propriedades************");
 			
 			Properties prop = getProp();
@@ -26,9 +29,21 @@ public class ConexaoBanco {
 			url = prop.getProperty("prop.server.url");
 			password = prop.getProperty("prop.server.password");
 			
-			System.out.println("Login = " + login);
-			System.out.println("Host = " + url);
-			System.out.println("Password = " + password);
+		
+			try {
+				Class.forName("org.h2.Driver");
+				Connection con = DriverManager.getConnection(url, login, password);
+				return con;
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				return null;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+			}
+				
 		}
-	
+		public static void main(String[] args) throws IOException {
+			new ConexaoBanco().abrirConexao();
+		}
 }
