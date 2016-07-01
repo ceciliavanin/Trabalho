3ba834aa482f1f105ada56e17217c881ac64ae24
Vanessa Panosso <panossovanessa@gmail.com>
2016-07-01 17:09:40 -0300

    Relatorio

--- Trabalho154143/src/main/java/br/univel/view/EmitirRelatorio.java
+++ Trabalho154143/src/main/java/br/univel/view/EmitirRelatorio.java
@@ -1,8 +1,27 @@
 package br.univel.view;
 
+import javax.swing.JFrame;
+
+import br.univel.DaoImp;
+import br.univel.ListaProdutos;
+import br.univel.reports.ProdutoJRDataSource;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+
 public class EmitirRelatorio {
 
 	public void relCliente(){
+		String arq = "Cliente.jasper";		
+		JasperPrint jp = JasperFillManager.fillReport(arq, null,.getCon());
+		
+		JasperViewer jasperViewer = new JasperViewer(jp);
+		
+		jasperViewer.setBounds(50, 50, 320, 240);
+		jasperViewer.setLocationRelativeTo(null);
+		jasperViewer.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		jasperViewer.setVisible(true);
 		
 	}
 	public void relProduto(){
